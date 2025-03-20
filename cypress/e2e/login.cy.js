ec968cc391a35473d07d9e0f6dcc6c600a5f122a
Wesley Cicote <wcicote@gmail.com>
2025-03-19 22:54:55 -0300

    feat: Username e Password vazio

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -8,4 +8,18 @@ describe('Login', () => {
     cy.get('.oxd-button').click()
     cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').should('have.text', 'Dashboard')
   })
+
+  it('Username Vazio', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+    cy.get('.oxd-button').click()
+    cy.get('.oxd-input-group > .oxd-text').should('have.text', 'Required')
+  })
+
+  it('Password Vazio', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+    cy.get('.oxd-button').click()
+    cy.get('.oxd-input-group > .oxd-text').should('have.text', 'Required')
+})
 })
