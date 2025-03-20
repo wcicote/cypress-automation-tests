2bebb66829e2adbac32367ee761f33ed19ff200b
Wesley Cicote <wcicote@gmail.com>
2025-03-19 23:23:20 -0300

    feat: Username e Password Incorreto e vazio

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -23,6 +23,13 @@ describe('Login', () => {
     cy.get('.oxd-input-group > .oxd-text').should('have.text', 'Required')
 })
 
+it('Username e Password Vazio', () => {
+  cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+  cy.get('.oxd-button').click()
+  cy.get(':nth-child(2) > .oxd-input-group > .oxd-text').should('have.text', 'Required' )
+  cy.get(':nth-child(3) > .oxd-input-group > .oxd-text').should('have.text', 'Required' )
+});
+
 it('Username Incorreto', () => {
   cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
     cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('aabbcc')
@@ -36,6 +43,15 @@ it('Password Incorreto', () => {
   cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
     cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
     cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin')
+    cy.get('.oxd-button').click()
+    cy.get('.oxd-alert-content > .oxd-text').should('have.text', 'Invalid credentials')
+    cy.get('.oxd-alert-content > .oxd-icon').should('be.visible')
+});
+
+it('Username e Password Incorreto', () => {
+  cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('adm')
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('adm')
     cy.get('.oxd-button').click()
     cy.get('.oxd-alert-content > .oxd-text').should('have.text', 'Invalid credentials')
     cy.get('.oxd-alert-content > .oxd-icon').should('be.visible')
