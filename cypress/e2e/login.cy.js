c2f6d3d213289ce62f1f621c6937b8b0add317e5
Wesley Cicote <wcicote@gmail.com>
2025-03-19 22:45:50 -0300

    feat: Login com Sucesso

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,7 +1,11 @@
 ///<reference types='cypress'/>
 
 describe('Login', () => {
-  it('login', () => {
+  it('login com Sucesso', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+    cy.get('.oxd-button').click()
+    cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').should('have.text', 'Dashboard')
   })
 })
